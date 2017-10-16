95337ccf5f1bbfe37eec59035b17d8b8d2015678
Cesar Caceres <cesarc770@gmail.com>
2017-10-15 22:47:11 -0600

    adding other pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,14 +25,14 @@ $("main-background").fadeOut(800);
 
 //div for mmain menu with links to other pages and links to github and linkedIn
 var menu = $("<div>");
-var linkedin = "<img src='assets/images/linkedin.png'>";
-var github = "<img src='assets/images/github.png'>";
-var mail = "<img src='assets/images/mail.png'>";
+var linkedin = "<a href='https://www.linkedin.com/in/cesar-caceres/'><img src='assets/images/linkedin.png'></a>";
+var github = "<a href='https://github.com/cesarc770'><img src='assets/images/github.png'></a>";
+var mail = "<a href='mailto:cesarc770@gmail.com'><img src='assets/images/mail.png'></a>";
 menu.html("<span class=\"glyphicon glyphicon-remove\" id=\"close\"></span>" +
 	"<ul class=\"text-center\">" +
 	        "<li><a href=\"skills.html\">SKILLS</a></li>" +
 	        "<li><a href=\"portfolio.html\">PORTFOLIO</a></li>" +
-	        "<li><a href=\"About\">ABOUT</a></li>" +
+	        "<li><a href=\"about.html\">ABOUT</a></li>" +
 	     "</ul>"
 	     +"<div class=\"row\"><div class='col-sm-4 links'>" + linkedin+ "</div><div class='col-sm-4 links'>"+ github+ "</div><div class='col-sm-4 links'>"+ mail +"</div></div>");
 menu.addClass("menu text-right");
@@ -52,24 +52,43 @@ $("#close").on("click", function(){
 //-----------------------------------------------------------------------------------------
 
 //function on mouseover the portfolio items 
-var flipped = false;
+var flippedHang = false;
+var flippedRPG = false;
 $("#hangman-btn").on("click", function(){
-	if(flipped === false){
+	if(flippedHang === false){
 		$("#hangman").addClass("animated flipInY");
 		$("#hangman").removeClass("flip");
-		$("#hangman").html("<div class='container-fluid text-justify'><h2>Hangman</h2><p>This Website was created to showcase my JavaScript and jQuery skills.</p>"+
-			" <p>JavaScript handles the logic of the game as well as some of the animation of the page. CSS elements are incorporated to give the page the selected team themes.</p>"+
-			"<p>The game is simple, but fun, a word is generated randomly and it takes inputs from your keyboard or the buttons. Give it a try and see how many words you can get right!</p></div>");
-		flipped = true;
+		$("#hangman").html("<div class='container-fluid text-justify'><h2>El \"Futbol\" Hangman</h2><p>This Website was created to showcase my JavaScript and jQuery skills.</p>"+
+		"<p>It's a fun game; a word is generated randomly and it takes inputs from your keyboard or the buttons. Give it a try and see how many words you can get right!</p>"+
+		"<p> Click <a href='https://github.com/cesarc770/Hangman-Game' target='_blank'>here</a> to see the code in GitHub</p> or "+
+		"<a href='http://www.cesaracaceres.com/Hangman-Game/' target='_blank'>Go to Site</a></div>");
+		flippedHang = true;
 	}else{
 		$("#hangman").addClass("flip");
 		$("#hangman").removeClass("flipInY");
 		$("#hangman").html("<img src=\"assets/images/hangman.png\" class=\"img-responsive\">")
-		flipped = false;
+		flippedHang = false;
 	}
 
 });
 
+$("#rpg-btn").on("click", function(){
+	if(flippedRPG === false){
+		$("#rpg").addClass("animated flipInY");
+		$("#rpg").removeClass("flip");
+		$("#rpg").html("<div class='container-fluid text-justify'><h2>Pokemon RPG</h2><p>This Website was created to showcase my JavaScript and jQuery skills.</p>"+
+		"<p>Pick your Pokemon, pick one to battle and beat them all. The base attack of the pokemon you choose increases by its own power's base each time you attack while your opponent's attack stays the same.</p>"+
+		"<p> Click <a href='https://github.com/cesarc770/RPG-Game' target='_blank'>here</a> to see the code in GitHub</p> or "+
+		"<a href='http://www.cesaracaceres.com/RPG-Game/' target='_blank'>Go to Site</a></div>");
+		flippedRPG = true;
+	}else{
+		$("#rpg").addClass("flip");
+		$("#rpg").removeClass("flipInY");
+		$("#rpg").html("<img src=\"assets/images/rpg.png\" class=\"img-responsive\">")
+		flippedRPG = false;
+	}
+
+});
 
 
 		
